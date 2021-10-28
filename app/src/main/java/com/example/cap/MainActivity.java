a292f57d8c20cfeb3a59d76acdd26db6e0f19a73
choihana <hana9213@naver.com>
2021-10-28 18:39:03 +0900

    n

--- app/src/main/java/com/example/cap/MainActivity.java
+++ app/src/main/java/com/example/cap/MainActivity.java
@@ -9,19 +9,19 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    /*BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
     Fragment1 fragment1;
-    Fragment2 fragment2;*/
+    Fragment2 fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*fragment1 = new Fragment1();
+        fragment1 = new Fragment1();
         fragment2 = new Fragment2();
 
-        getSupportFragmentManager().beginTransaction().
+        /*getSupportFragmentManager().beginTransaction().
                 replace(R.id.main_layout,fragment1).commitAllowingStateLoss();*/
 
         /*BottomNavigationView bottimNavigationView = findViewById(R.id.bottom);
